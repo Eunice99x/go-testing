9d298611b364afc26bbfeb120023110416127106
Younes Outerbah <younesouterbah1@gmail.com>
2024-09-11 02:08:15 +0100

    bitcoin

---
+++ bitcoin/bitcoin_test.go
@@ -0,0 +1,65 @@
+package bitcoin
+
+import (
+	"testing"
+)
+
+/*
+this package:
+	go install github.com/kisielk/errcheck@latest
+	using this command
+	errcheck .
+	will check for unchecked error
+*/
+func TestWallet(t *testing.T){
+
+	 assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	assertError := func(t testing.TB, got, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, Bitcoin(20))
+	})
+}
+
